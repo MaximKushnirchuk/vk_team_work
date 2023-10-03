6c806d8dad593ffac8258ff70762f8e187f9fc73
Strasse <jeka_krsk@mail.ru>
2023-10-03 21:49:05 +0700

    bot_class added

---
+++ main.py
@@ -0,0 +1,9 @@
+# https://github.com/netology-code/adpy-team-diplom/blob/main/README.md
+
+from bot_class import Bot
+
+if __name__ == "__main__":
+    while True:
+        user = Bot()
+        user.listen()
+        # user.auth_user()
