a41b73a96bef0746ca528d1196c9935b566962ba
Strasse <jeka_krsk@mail.ru>
2023-10-12 20:33:14 +0700

    bot_class changed v1.3

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://github.com/netology-code/adpy-team-diplom/blob/main/README.md
+# https://vk.com/club222742957
+
+from bot_class import Bot
+
+if __name__ == "__main__":
+    while True:
+        Bot().initialization()
